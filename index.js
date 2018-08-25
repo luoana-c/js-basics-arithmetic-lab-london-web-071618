329e3bc655376c39c52b1ab4662d965b38379b8f
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-25 10:13:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,4 @@ const newID = oldID + 1000000000;
 const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.floor(Math.random() * 20) + 1;
 const randomInteger = Math.floor(randomNumber);
+const randomUserID = Math.floor(Math.random() * 1000000020) +1; 
